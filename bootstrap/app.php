6801407104d3721af072090ade825bf884a586d7
alizufan <ali.zufan@sprintasia.co.id>
2021-03-19 14:05:30 +0700

    update app

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -75,9 +75,9 @@ $app->configure('queue');
 |
  */
 
-// $app->middleware([
-//     App\Http\Middleware\ExampleMiddleware::class
-// ]);
+$app->middleware([
+    App\Http\Middleware\CorsMiddleware::class,
+]);
 
 $app->routeMiddleware([
     'auth.private' => App\Http\Middleware\AuthPrivateMiddleware::class,
