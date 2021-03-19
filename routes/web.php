93f8f34a94ca4f65efd04c3667f5d5d0d531bf6e
alizufan <ali.zufan@sprintasia.co.id>
2021-03-19 14:04:48 +0700

    update route

--- routes/web.php
+++ routes/web.php
@@ -20,33 +20,42 @@ $router->get('/', function () use ($router) {
 $router->group(["prefix" => "api"], function () use ($router) {
     $router->group(["prefix" => "v1"], function () use ($router) {
 
-        $router->group(["namespace" => "Auth", "prefix" => "auth"], function () use ($router) {
-            $router->post("login", "LoginController@Handler");
-        });
-
         $router->group(["namespace" => "Auth"], function () use ($router) {
-            $router->get("permissions", "PermissionController@GetAllPermissionHandler");
-            $router->get("groups", "GroupController@GetHandler");
-            $router->get("roles", "RoleController@GetRoleHandler");
+            $router->get("permissions", "PermissionController@GetAllPermissionRawHandler");
+            $router->get("list-permissions", "PermissionController@GetAllPermissionHandler");
+            $router->get("update-permissions", "PermissionController@UpdatePermissionHandler");
+
+            $router->group(["prefix" => "auth"], function () use ($router) {
+                $router->post("login", "LoginController@Handler");
+                $router->post("change-password", ["middleware" => ["auth.private"], "uses" => "UserController@ChangeUserPasswordHandler"]);
+            });
+
+            $router->get("groups", ["middleware" => ["auth.private"], "uses" => "GroupController@GetGroupHandler"]);
+            $router->group(["prefix" => "group", "middleware" => ["auth.private"]], function () use ($router) {
+                $router->get("/{id:[0-9]+}", "GroupController@GetGroupHandler");
+                $router->post("add", "GroupController@AddGroupHandler");
+                $router->post("update/{id:[0-9]+}", "GroupController@EditGroupHandler");
+            });
+
+            $router->get("roles", ["middleware" => ["auth.private"], "uses" => "RoleController@GetRoleHandler"]);
+            $router->group(["prefix" => "role", "middleware" => ["auth.private"]], function () use ($router) {
+                $router->get("/{id:[0-9]+}", "RoleController@GetRoleHandler");
+                $router->post("add", "RoleController@AddRoleHandler");
+                $router->post("update/{id:[0-9]+}", "RoleController@EditRoleHandler");
+            });
+
+            $router->get("users", ["middleware" => ["auth.private"], "uses" => "UserController@GetUserHandler"]);
+            $router->group(["prefix" => "user", "middleware" => ["auth.private"]], function () use ($router) {
+                $router->get("/{id:[0-9]+}", "UserController@GetUserHandler");
+                $router->post("add", "UserController@AddUserHandler");
+                $router->post("update/{id:[0-9]+}", "UserController@EditUserHandler");
+            });
         });
 
-        $router->group(["namespace" => "Auth", "prefix" => "group"], function () use ($router) {
-            $router->post("add", "GroupController@AddHandler");
-            $router->post("update/{id:[0-9]+}", "GroupController@EditHandler");
-        });
-
-        $router->group(["namespace" => "Auth", "prefix" => "role"], function () use ($router) {
-            $router->post("add", "RoleController@AddRoleHandler");
-            $router->post("update/{id:[0-9]+}", "RoleController@EditRoleHandler");
-        });
-
-        $router->group(["namespace" => "Auth", "prefix" => "user"], function () use ($router) {
-            $router->post("add", "UserController@AddUserHandler");
-        });
-
-        $router->group(["prefix" => "siap"], function () use ($router) {
+        $router->group(["namespace" => "SIAP", "prefix" => "siap", "middleware" => ["auth.private"]], function () use ($router) {
             $router->get("dispositions", "DispositionController@GetLetterHandler");
-            $router->group(["namespace" => "SIAP", "prefix" => "disposition"], function () use ($router) {
+
+            $router->group(["prefix" => "disposition"], function () use ($router) {
                 $router->post("add", "DispositionController@AddNewLetterHandler");
                 $router->post("update/{id:[0-9]+}", "DispositionController@EditLetterHandler");
             });
