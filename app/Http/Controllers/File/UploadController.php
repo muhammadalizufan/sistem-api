1111de4b7763f6afd62e59d0a53294cd98e388c8
alizufan <ali.zufan@sprintasia.co.id>
2021-03-27 19:46:56 +0700

    update controller

--- app/Http/Controllers/File/UploadController.php
+++ app/Http/Controllers/File/UploadController.php
@@ -2,6 +2,8 @@
 namespace App\Http\Controllers\File;
 
 use App\Http\Controllers\Controller;
+use App\Libs\Helpers;
+use App\Models\Extension\File;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\URL;
 
@@ -10,19 +12,35 @@ class UploadController extends Controller
     public function Handler(Request $r)
     {
         $this->validate($r, [
-            'file' => 'required|max:15360', // mimes:jpg,jpeg,bmp,png // max: 15 MB
+            'file' => 'required|max:15360', // anything but max size: 15 MB
+            'from' => 'required|integer|min:1',
         ]);
         $file = $r->file("file");
-        $path = base_path('/public/storage');
+
+        $basepath = '/public/storage';
+        $path = base_path($basepath);
         if (!file_exists($path)) {
             mkdir($path, 0777, true);
         }
-        $filename = time() . "-" . $file->getClientOriginalName();
-        $file->move($path, $filename);
+
+        $ext = $file->extension();
+        $name = time() . "-" . Helpers::QuickRandom();
+        $fullname = $name . ".$ext";
+        $file->move($path, $fullname);
+
+        $F = File::create([
+            'name' => $name,
+            'fullname' => $fullname,
+            'ref_type' => $r->input('from', 0),
+            'ref_id' => 0,
+            'ext' => $ext,
+            'path' => $basepath,
+        ]);
 
         return response([
-            'file' => $filename,
-            'url' => URL::to('/storage/' . $filename),
+            'id' => $F->id ?? 0,
+            'file' => $name,
+            'url' => URL::to('/storage/' . $fullname),
         ]);
     }
 }
