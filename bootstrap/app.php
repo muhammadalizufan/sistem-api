f57f52fb33ba53764b9e0b564792df8b2f5466cd
alizufan <ali.zufan@sprintasia.co.id>
2021-03-31 18:14:14 +0700

    cors

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -8,6 +8,10 @@ require_once __DIR__ . '/../vendor/autoload.php';
 
 date_default_timezone_set(env('APP_TIMEZONE', 'UTC'));
 
+header('Access-Control-Allow-Origin: *');
+header('Access-Control-Allow-Methods: *');
+header('Access-Control-Allow-Headers: Origin, X-Requested-With,Authorization, Content-Type, Accept');
+
 /*
 |--------------------------------------------------------------------------
 | Create The Application
